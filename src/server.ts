ce8597e63b183b34818846200fddebf23da3e4e0
Davi Batista <davideosmar11@gmail.com>
2023-12-30 09:46:28 -0300

    config(server): added basic config to the API

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response, request } from 'express'
 import http from 'http'
 import mongoose from 'mongoose'
 import { config } from './config/config'
@@ -7,9 +7,65 @@ import Logging from './library/Logging'
 const router = express()
 
 /* Connection to Atlas */
-mongoose.connect(config.mongo.url)
-.then(() => {
-    Logging.info("Connected to MongoDB remotely.")
-}).catch((err) => {
-    Logging.err("Error while trying to connect to MongoDB remotely.")
+mongoose
+    .connect(config.mongo.url, { retryWrites: true, w: "majority" })
+    .then(() => {
+        Logging.info("Connected to MongoDB remotely.")  
+        startServer()
+    })
+    .catch((err) => {
+        Logging.err("Error while trying to connect to MongoDB remotely.")
 })
+
+const startServer = () => {
+    router.use((request: Request, response: Response, next: NextFunction) => {
+
+        /* Log the request */
+        Logging.info(`Incoming => Method: [${request.method}] | URL:[${request.url}] | IP: [${request.socket.remoteAddress}]`)
+
+        response.on("finish", () => {
+            /* Log the response */
+            Logging.info(`Outgoing => Method: [${request.method}] | URL:[${request.url}] | IP: [${request.socket.remoteAddress}] | Status: [${response.statusCode}]`)
+        })
+
+        next()
+    })
+
+    router.use(express.urlencoded({ extended: true }))
+    router.use(express.json())
+
+    /* Rules of the API */
+    router.use((request: Request, response: Response, next: NextFunction) => {
+        response.header("Access-Control-Allow-Origin", "*")
+        response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+        
+        if (request.method == "OPTIONS") {
+            response.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
+            return response.status(200).json({})
+        }
+
+        next()
+    })
+
+    /* Routes */
+
+    /* Health Check */
+    router.get("/ping", (request: Request, response: Response) => {
+        response.status(200).json({
+            message: "pong"
+        })
+    })
+
+    /* Error handling */
+    router.use((request: Request, response: Response, next: NextFunction) => {
+        const error = new Error("Not found")
+
+        return response.status(404).json({
+            message: error.message
+        })
+    })
+
+    http.createServer(router).listen(config.server.port, () => {
+        Logging.info(`Server running on port ${config.server.port}`)
+    })
+}
