5282a6104631d5191387ecd53bfd4ca77ccc5ee0
Davi Batista <davideosmar11@gmail.com>
2024-01-03 22:34:16 -0300

    env(var): changed env-var configs on config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,15 +1,17 @@
 import * as env from "env-var"
 
-const DB_URL = env.get("DB_URL").required().asString()
-const PORT = env.get("PORT").required().asString()
-export const JWT_KEY = env.get("JWT_KEY").required().asString()
+export const DB_URL: string = env.get("DB_URL").required().asString()
+
+export const PORT: string = env.get("PORT").required().asString()
+
+export const JWT_KEY: string = env.get("JWT_KEY").required().asString()
 
 export const config = {
     mongo: {
-        url: DB_URL || ""
+        url: DB_URL
     },
 
     server: {
-        port: PORT || 3000
+        port: PORT
     }
 }
