d8efebda2c7d5c5906e3d72719de581eb6e853ee
Davi Batista <davideosmar11@gmail.com>
2024-01-03 22:13:30 -0300

    logging(Category): added logging messages to category

--- src/controllers/Category.ts
+++ src/controllers/Category.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, request } from "express";
 import mongoose from "mongoose"
-import Category from "../models/Category";
+import Category, { ICategory } from "../models/Category";
+import Logging from "../library/Logging";
 
 /* Create a new category */
 const createCategory = (request: Request, response: Response, next: NextFunction) =>    {
@@ -13,8 +14,16 @@ const createCategory = (request: Request, response: Response, next: NextFunction
     })
 
     return category.save()
-        .then((category) => response.status(201).json({ category }))
-        .catch((error) => response.status(500).json({ error }))
+
+        .then((category) => {
+            response.status(201).json({ category })
+            Logging.data(`Nova categoria registrada: ${category.name}`)
+        })
+
+        .catch((error) => {
+            response.status(500).json({ error })
+            Logging.err(`Tentativa de criação de categoria mau sucedida.`)
+        })
 }
 
 /* Finding a category by its id */
@@ -22,15 +31,28 @@ const getCategory = (request: Request, response: Response, next: NextFunction) =
     const categoryId = request.params.categoryId
 
     return Category.findById(categoryId)
+
         .then((category) => (category ? response.status(200).json({ category }) : response.status(404).json({ message: 'not found' })))
-        .catch((error) => response.status(500).json({ error }));
+
+        .catch((error) => {
+            response.status(500).json({ error })
+            Logging.err(`Tentativa de busca de categoria mau sucedida.`)
+        });
 }
 
 /* Get all the categories */
 const getAllCategories = (request: Request, response: Response, next: NextFunction) => {
     return Category.find()
-        .then((categories) => response.status(200).json({ categories }))
-        .catch((error) => response.status(500).json({ error }));
+
+        .then((categories) => {
+            response.status(200).json({ categories })
+            Logging.data("Busca de categorias retornada com sucesso.")
+        })
+
+        .catch((error) => {
+            response.status(500).json({ error })
+            Logging.err("Erro ao retornar categorias.")
+        });
 }
 
 /* Edit a category passing its id */
@@ -38,19 +60,33 @@ const editCategory = (request: Request, response: Response, next: NextFunction) 
     const categoryId = request.params.categoryId
 
     return Category.findById(categoryId)
-    .then((category) => {
+
+    .then((category: any) => {
         if (category) {
             category.set(request.body);
 
             return category
                 .save()
-                .then((category) => response.status(201).json({ category }))
-                .catch((error) => response.status(500).json({ error }));
+
+                .then((category: any) => {
+                    response.status(201).json({ category })
+                    Logging.data(`Categoria ${category.name} atualizada com sucesso.`)
+                })
+
+                .catch((error: Error) => {
+                    response.status(500).json({ error })
+                    Logging.err(`Tentativa de atualização de categoria mau sucedida.`)
+                });
         } else {
+            Logging.err(`Tentativa de atualização de categoria mau sucedida. Motivo: Categoria não encontrada.`)
             return response.status(404).json({ message: 'not found' });
         }
     })
-    .catch((error) => response.status(500).json({ error }));
+
+    .catch((error) => {
+        Logging.err(`Tentativa de atualização de categoria mau sucedida.`)
+        response.status(500).json({ error })
+    });
 }
 
 /* Deleting a category passing its id */
@@ -58,8 +94,21 @@ const deleteCategory = (request: Request, response: Response, next: NextFunction
     const userId = request.params.categoryId
 
     return Category.findByIdAndDelete(userId)
-        .then((category) => (category ? response.status(201).json({ category, message: 'Deleted' }) : response.status(404).json({ message: 'not found' })))
-        .catch((error) => response.status(500).json({ error }));
+
+        .then((category: any) => {
+            if (category)   {
+                response.status(201).json({ category, message: 'Deleted' })
+                Logging.data(`Categoria ${category.name} deletada com sucesso.`)
+            }   else    {
+                response.status(404).json({ message: 'not found' })
+                Logging.err(`Tentativa de deleção de categoria mau sucedida. Motivo: Categoria não encontrada.`)
+            }
+        })
+
+        .catch((error) => {
+            response.status(500).json({ error })
+            Logging.err(`Tentativa de deleção de categoria mau sucedida.`)
+        });
 }
 
 export default { 
