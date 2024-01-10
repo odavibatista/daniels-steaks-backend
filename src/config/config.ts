207f6517f8b44db2b3de5c37db65eedc490b0e36
Davi Batista <davideosmar11@gmail.com>
2024-01-10 17:59:43 -0300

    config(env): added more environment variables for safety on database data

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,16 @@
 import * as env from "env-var";
 
-export const DB_URL: string = env.get("DB_URL").required().asString();
+export const DB_ENGINE: string = env.get("DB_ENGINE").required().asString();
+
+export const DB_SERVER: string = env.get("DB_SERVER").required().asString();
+
+export const DB_USER: string = env.get("DB_USER").required().asString();
+
+export const DB_PASSWORD: string = env.get("DB_PASSWORD").required().asString();
+
+export const DB_CLUSTER: string = env.get("DB_CLUSTER").required().asString();
+
+export const DATABASE: string = env.get("DATABASE").required().asString();
 
 export const PORT: string = env.get("PORT").required().asString();
 
@@ -8,12 +18,14 @@ export const JWT_KEY: string = env.get("JWT_KEY").required().asString();
 
 export const ADMIN_KEY: string = env.get("ADMIN_KEY").required().asString();
 
+export const connectionString: string = `${DB_ENGINE}+${DB_SERVER}://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DATABASE}`
+
 export const config = {
   mongo: {
-    url: DB_URL,
+    url: connectionString
   },
 
   server: {
-    port: PORT,
+    port: PORT
   },
 };
